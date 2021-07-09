5f266f743cf858f62ed39b089a6a4ebd73524624
Nicholas <nikolovnickv@gmail.com>
2021-07-08 23:23:41 -0500

    testing

--- src/Euler.scala
+++ src/Euler.scala
@@ -1,6 +1,5 @@
 import scala.collection.immutable.Range
-//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-//Find the sum of all the multiples of 3 or 5 below 1000.
+
 
 object Euler extends App{
 
@@ -8,6 +7,8 @@ object Euler extends App{
   val output = natural_number_multiples(1000)
   print(output)
 
+  //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+  //Find the sum of all the multiples of 3 or 5 below 1000.
   def natural_number_multiples( multipleValue : Int) : Int = {
     import scala.collection.mutable.ListBuffer
     var x =0;
